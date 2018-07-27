a8195d357a230e0236df4bad4c60ce0af485cf01
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2018-07-28 00:00:25 +0200

    Creating Your First Service - Creating Reusable Angular Services

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,11 +4,13 @@ import { EventsAppComponent} from "./events.app.component";
 import { EventsListComponent } from "./events/events-list.component";
 import { EventsThumbnailComponent } from "./events/event-thumbnail.component";
 import { NavBarComponent } from "./nav/navbar.component";
+import { EventService } from "./events/shared/event.service";
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [EventsAppComponent, EventsListComponent, EventsThumbnailComponent, NavBarComponent],
-    bootstrap: [EventsAppComponent]
+    bootstrap: [EventsAppComponent],
+    providers: [EventService]
 })
 
 export class AppModule {}
