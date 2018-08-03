a9ef0fa2af1c9be34424873257923eb27bf03a73
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2018-08-03 20:06:10 +0200

    Diving Deeper into Template-based Forms

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,8 +9,9 @@ import { Error404Component } from "./errors/404.component";
 import { EventsListComponent, EventsThumbnailComponent ,EventService, EventDetailsComponent, CreateEventComponent
 , EventRouterActivator, EventListResolver} from './events/index'
 import { AuthService } from "./user/auth.service";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 @NgModule({
-    imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+    imports: [BrowserModule, RouterModule.forRoot(appRoutes), FormsModule, ReactiveFormsModule],
     declarations: [EventsAppComponent, EventsListComponent,
         EventsThumbnailComponent, NavBarComponent, EventDetailsComponent, CreateEventComponent, Error404Component],
     bootstrap: [EventsAppComponent],
