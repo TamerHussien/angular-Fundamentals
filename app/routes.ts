7589ed9577a4cef737088f2fc31ef40fe3e98c65
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2018-08-03 12:22:13 +0200

    Organizing Your Exports with Barrels

--- app/routes.ts
+++ app/routes.ts
@@ -1,18 +1,12 @@
 import { Routes } from '@angular/router'
-
-import { EventsListComponent } from "./events/events-list.component";
-import { EventDetailsComponent } from "./events/event-details/event-details.component";
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouterActivator } from './events/event-details/event-router-activator';
-import { EventListResolver } from './events/event-list-resolver';
-
+import {EventsListComponent, EventDetailsComponent, CreateEventComponent, EventRouterActivator, EventListResolver} from './events/index'
 
 export const appRoutes: Routes = [
     {path : 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent']},
     {path : 'events', component: EventsListComponent, resolve:{events: EventListResolver}},
     {path : 'events/:id', component: EventDetailsComponent, canActivate: [EventRouterActivator]},
     {path : '404', component: Error404Component},
-    {path : '',redirectTo: '/events' , pathMatch: 'full'}
+    {path : '',redirectTo: '/events' , pathMatch: 'full'},
     {path: 'user', loadChildren: 'app/user/user.module#UserModule'}
 ]
