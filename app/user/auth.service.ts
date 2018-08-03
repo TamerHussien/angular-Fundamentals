a806834efe0e3977820b8762bdd9e1a2cbaa8e8a
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2018-08-03 18:00:16 +0200

    Creating Your First Reactive Form

--- app/user/auth.service.ts
+++ app/user/auth.service.ts
@@ -17,7 +17,10 @@ export class AuthService {
         }
 
     }
-
+updateCurrentUser(firstName: string, lastName: string){
+this.currentUser.firstName = firstName;
+this.currentUser.lastName = lastName;
+}
     isAuthenticated() {
         return !!this.currentUser;
     }
