10cb3c7e41807fb276edd383acf586c42b25ddca
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2018-08-16 02:46:32 +0200

    Creating Settings on Components

--- app/common/simpleModal.component.ts
+++ app/common/simpleModal.component.ts
@@ -29,12 +29,16 @@ export class SimpleModalComponent {
 
     @Input() title: string;
     @Input() elementId: string;
+    @Input() closeOnBodyClick: string;
+
     @ViewChild('modalContainer') modalContainer: ElementRef;
 
     constructor(@Inject(JQ_TOKEN) private $: any) {}
 
     closeModal() {
+        if(this.closeOnBodyClick.toLocaleLowerCase() === 'true'){
         this.$(this.modalContainer.nativeElement).modal('hide');
+        }
     }
 
 }
