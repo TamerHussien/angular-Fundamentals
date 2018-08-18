f887a9bd293d68e98f854682ddea8252907a05e0
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2018-08-18 23:54:04 +0200

    ntegrating Authentication with the Server

--- app/user/auth.service.ts
+++ app/user/auth.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from "@angular/core";
 import { IUser } from "./user.model";
+import { Http,Headers, RequestOptions } from "@angular/http";
+import { Observable } from "rxjs/Rx";
 
 @Injectable()
 
 export class AuthService {
+    constructor(private http:Http){}
 
     currentUser: IUser;
 
     loginUser(userName: string, password: string){
 
-        this.currentUser= {
-            id: 1,
-            userName: userName,
-            firstName: 'Tamer',
-            lastName: 'Hussien',
-        }
-
+        let headers = new Headers({'Content-Type': 'application/json'});
+      let options = new RequestOptions({headers: headers})
+        let loginInfo = {username: userName, password: password}
+        return this.http.post('/api/login', JSON.stringify(loginInfo), options).do(res => {
+            if (res) {
+                this.currentUser = res.json().user;
+            }
+        }).catch(error => {
+            return Observable.of(false)
+        })
     }
 updateCurrentUser(firstName: string, lastName: string){
 this.currentUser.firstName = firstName;
