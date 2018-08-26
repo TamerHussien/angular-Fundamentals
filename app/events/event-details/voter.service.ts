ae1c9218007d01cb5718aafffe96c5a149ec05bb
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2018-08-26 21:48:31 +0200

    Tuning Your rxJS Requests

--- app/events/event-details/voter.service.ts
+++ app/events/event-details/voter.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ISessions } from "../shared";
 import { Http,Headers, RequestOptions, Response } from "@angular/http";
-import { Observable } from "rxjs/Rx";
+import { Observable } from "rxjs/Observable";
 
 
 @Injectable()
